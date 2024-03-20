281d04be35eee103c58e3bcdcb44a13fd887a05f
truonglong1410 <2051010178long@ou.edu.vn>
2024-03-19 21:16:59 -0700

    sửa lại giao diện các phần còn lại và bổ sung giao diện thống kê sản phẩm bằng chart

---
+++ QLBH/Controllers/ThongKeThietBiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBH.Controllers
+{
+    public class ThongKeThietBiController : Controller
+    {
+        // GET: ThongKeThietBi
+        public ActionResult ThongKeTB()
+        {
+            return View();
+        }
+    }
+}
