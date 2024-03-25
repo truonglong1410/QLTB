f9a60e9e4d62ac40cf121f1b0b025749b61743d9
truonglong1410 <2051010178long@ou.edu.vn>
2024-03-24 21:32:36 -0700

    sửa 1 số chức năng

---
+++ QLBH/Controllers/PhanQuyenNguoiDungController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLBH.Controllers
+{
+    public class PhanQuyenNguoiDungController : Controller
+    {
+        // GET: PhanQuyenNguoiDung
+        public ActionResult PhanQuyenND()
+        {
+            return View();
+        }
+       
+    }
+}
